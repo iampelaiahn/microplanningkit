edd6624b4339699dcb2375273896b69feedcf176
Pelaiah Ngarande <iampukkynotto@gmail.com>
2026-02-16 11:14:08 +0000

    remove this duplicate sidebar (_for element <Navigation>_)

--- src/app/field/profiling/layout.tsx
+++ src/app/field/profiling/layout.tsx
@@ -1,17 +1,7 @@
-
-import { Navigation } from '@/components/Navigation'
-
 export default function ProfilingLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <>
-      <Navigation />
-      <main className="flex-1 p-6 pb-24 md:pb-6 overflow-auto bg-background">
-        {children}
-      </main>
-    </>
-  )
+  return <>{children}</>
 }
